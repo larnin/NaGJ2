1bc994f198324ad4d214b43ed33c204c09188242
Nico <nicolas-donald.laurent@laposte.net>
2023-02-22 23:44:39 +0100

    Tower script WIP

---
+++ Assets/Scripts/DebugCursorObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DebugCursorObject : MonoBehaviour
+{
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane p = new Plane(new Vector3(0, 1, 0), transform.position);
+        float enter = 0;
+        if (p.Raycast(ray, out enter))
+        {
+            Vector3 pos = ray.GetPoint(enter);
+            transform.position = pos;
+        }
+    }
+}

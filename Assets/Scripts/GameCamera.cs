08895f782220ccbf4460db91aaae177a12e9ff57
Nico <nicolas-donald.laurent@laposte.net>
2023-02-26 12:07:57 +0100

    Camera & ui

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] float m_minZoom = 25;
+    [SerializeField] float m_maxZoom = 1;
+    [SerializeField] float m_stepZoom = 1.1f;
+
+    Camera m_camera;
+    float m_initialZoom;
+    Vector3 m_initialPosition;
+
+    Vector3 m_oldMousePos;
+
+    private void Start()
+    {
+        m_camera = Camera.main;
+        m_initialPosition = transform.position;
+        m_initialZoom = m_camera.orthographicSize;
+    }
+
+    private void Update()
+    {
+        float scrollY = Input.mouseScrollDelta.y;
+        float multiplier = MathF.Pow(m_stepZoom, MathF.Abs(scrollY));
+        if (MathF.Sign(scrollY) < 0)
+            multiplier = 1/multiplier;
+
+        float newSize = m_camera.orthographicSize * multiplier;
+        if (newSize > m_maxZoom && newSize < m_minZoom)
+            m_camera.orthographicSize = newSize;
+
+        if(Input.GetMouseButton(2))
+        {
+            var oldRay = m_camera.ScreenPointToRay(m_oldMousePos);
+            var newRay = m_camera.ScreenPointToRay(Input.mousePosition);
+
+            Plane p = new Plane(Vector3.up, Vector3.zero);
+
+            float enter;
+            p.Raycast(oldRay, out enter);
+            Vector3 oldPos = oldRay.GetPoint(enter);
+
+            p.Raycast(newRay, out enter);
+            Vector3 newPos = newRay.GetPoint(enter);
+
+            Vector3 delta = newPos - oldPos;
+
+            Vector3 currentPos = m_camera.transform.position;
+            currentPos -= delta;
+            m_camera.transform.position = currentPos;
+        }
+
+        m_oldMousePos = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    enum ButtonType
+    {
+        ground,
+        tower0,
+        tower1,
+        tower2,
+        trashCan,
+    }
+
+    class ButtonInfo
+    {
+        public Button button;
+        public TMP_Text cost;
+    }
+
+    int currentButton = -1;
+    ButtonInfo[] m_buttons;
+
+    TMP_Text m_populationText;
+    TMP_Text m_moneyText;
+
+    TMP_Text m_waveLabelText;
+    TMP_Text m_waveDescriptionText;
+
+    private void Start()
+    {
+        //explore that shit of hierarchy !
+
+        var back = transform.Find("Back");
+        if (back == null)
+            return;
+        var pop = back.Find("Population");
+        if (pop != null)
+            m_populationText = pop.Find("Value")?.GetComponent<TMP_Text>();
+        var money = back.Find("Money");
+        if (money != null)
+            m_moneyText = money.Find("Value")?.GetComponent<TMP_Text>();
+
+        m_buttons = new ButtonInfo[Enum.GetValues(typeof(ButtonType)).Length];
+        for (int i = 0; i < m_buttons.Length; i++)
+            m_buttons[i] = new ButtonInfo();
+
+        m_buttons[(int)ButtonType.ground].button = back.Find("GroundButton")?.GetComponent<Button>();
+        m_buttons[(int)ButtonType.tower0].button = back.Find("Tower0Button")?.GetComponent<Button>();
+        m_buttons[(int)ButtonType.tower1].button = back.Find("Tower1Button")?.GetComponent<Button>();
+        m_buttons[(int)ButtonType.tower2].button = back.Find("Tower2Button")?.GetComponent<Button>();
+        m_buttons[(int)ButtonType.trashCan].button = back.Find("DeleteButton")?.GetComponent<Button>();
+        
+        for(int i = 0; i < m_buttons.Length; i++)
+        {
+            if (m_buttons[i].button == null)
+                return; //stop here, we must have found every button
+            m_buttons[i].cost = m_buttons[i].button.transform.Find("Cost")?.GetComponent<TMP_Text>();
+        }
+
+        m_buttons[(int)ButtonType.ground].button.onClick.AddListener(OnClickGroundButton);
+        m_buttons[(int)ButtonType.tower0].button.onClick.AddListener(OnClickTower0Button);
+        m_buttons[(int)ButtonType.tower1].button.onClick.AddListener(OnClickTower1Button);
+        m_buttons[(int)ButtonType.tower2].button.onClick.AddListener(OnClickTower2Button);
+        m_buttons[(int)ButtonType.trashCan].button.onClick.AddListener(OnClickTrashBinButton);
+
+        var wave = back.Find("WaveBack");
+        if(wave != null)
+        {
+            m_waveLabelText = wave.Find("NextWaveLabel")?.GetComponent<TMP_Text>();
+            m_waveDescriptionText = wave.Find("NextWaveText")?.GetComponent<TMP_Text>();
+        }
+    }
+
+    void OnClickGroundButton()
+    {
+        ResetAllButtons();
+        m_buttons[(int)ButtonType.ground].button.interactable = false;
+
+    }
+
+    void OnClickTower0Button()
+    {
+        ResetAllButtons();
+        m_buttons[(int)ButtonType.tower0].button.interactable = false;
+
+
+    }
+
+    void OnClickTower1Button()
+    {
+        ResetAllButtons();
+        m_buttons[(int)ButtonType.tower1].button.interactable = false;
+
+
+    }
+
+    void OnClickTower2Button()
+    {
+        ResetAllButtons();
+        m_buttons[(int)ButtonType.tower2].button.interactable = false;
+
+
+    }
+
+    void OnClickTrashBinButton()
+    {
+        ResetAllButtons();
+        m_buttons[(int)ButtonType.trashCan].button.interactable = false;
+
+
+    }
+
+    void ResetAllButtons()
+    {
+        for (int i = 0; i < m_buttons.Length; i++)
+            m_buttons[i].button.interactable = true;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void UpdatePopAndMoney()
+    {
+
+    }
+
+    void UpdateWave()
+    {
+
+    }
+}
